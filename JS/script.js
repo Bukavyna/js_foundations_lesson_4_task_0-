629e2d8706b1cffd659568740246e95e51ff3039
Taras <luchakt2@gmail.com>
2023-09-15 16:01:14 +0300

    finish

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
 
+let count = 4;
+let a = false;
+
+while (count > 0 && !a) {
+	let password = prompt("Введіть пароль:");
+
+	if (password == 4) {
+		a = 4;
+		document.write(`<h1>Ласкаво просимо на сайт</h1>`);
+	} else {
+		count--;
+		if (count > 0) {
+			prompt(`Невірний пароль. Залишилось ${count} спроби.`);
+		} else {
+			prompt("Будь ласка, спробуйте пізніше!");
+		}
+	}
+}
